e65cb1d225fe0e312ea111d247afe1336e781d8d
ThamesDev <freddie@jemison.co.uk>
2021-06-25 16:51:20 +0100

    Fix formatting

--- usr/bin/local-prune.sh
+++ usr/bin/local-prune.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $PWD || exit
-git checkout "$1" || printf "This error message means that either\n1. you ran this command outside a git repository, or\n2. you didn't specify the primary branch. local-prune must be run in the format\n$ local-prune [main-branch]\n where [main-branch] is your repo's primary branch. It may be called \"main\", \"primary\", or if you're looking at an old repo, \"master\". It can be found by typing\n$ git branch\n into the terminal. Execute the command again with this argument.\n"; exit
-git fetch -p || printf "This message means that you haven't specified your origin/upstream repo! Do that, then run this command again.\n"; exit
+git checkout "$1" || printf "\nlocal-prune report:\n\nThis error message means that either\n1. you ran this command outside a git repository, or\n2. you didn't specify the primary branch. local-prune must be run in the format\n$ local-prune [main-branch]\n where [main-branch] is your repo's primary branch. It may be called \"main\", \"primary\", or if you're looking at an old repo, \"master\". It can be found by typing\n$ git branch\n into the terminal. Execute the command again with this argument.\n"; exit
+git fetch -p || printf "\nlocal-prune report:\n\nThis message means that you haven't specified your origin/upstream repo! Do that, then run this command again.\n"; exit
 git pull
-git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -d || printf "This means that there are no deleted remote branches which local branches were tracking.\n"; exit
+git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -d || printf "\nlocal-prune report:\n\nThis means that there are no deleted remote branches which local branches were tracking.\n"; exit
